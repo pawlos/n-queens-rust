409af718021ba959a435b4398be71fdbef0400b4
Paweł Łukasik <lukasik.pawel@gmail.com>
2023-05-23 20:46:45 +0200

    Implement serializaing board

--- src/serialize.rs
+++ src/serialize.rs
@@ -1,5 +1,22 @@
-pub fn serialize(board: Vec<usize>) -> Vec<String> {
-    vec![String::from("Q")]
+pub fn serialize(board: &Vec<usize>) -> Vec<String> {
+    let mut result = vec![];
+    for y in 0..board.len() {
+        let mut row = String::new();
+        let xx = board.iter()
+        .enumerate().filter(|(x,yy)| yy == &&y)
+        .last().unwrap().0;
+        for x in 0..board.len() {
+            if x == xx {
+                row.push('Q')
+            }
+            else {
+                row.push('.');
+            }
+        }
+        result.push(row);
+    }
+
+    result
 }
 
 #[cfg(test)]
@@ -10,8 +27,20 @@ mod tests {
     fn one_size_board() {
         let board = vec![0];
 
-        let result = serialize(board);
+        let result = serialize(&board);
 
         assert_eq!(result, vec!["Q"])
+    }
+
+    #[test]
+    fn four_size_board() {
+        let board = vec![1,3,0,2];
+
+        let result = serialize(&board);
+
+        assert_eq!(result, vec!["..Q.",
+                                "Q...",
+                                "...Q",
+                                ".Q.."])
     }
 }
