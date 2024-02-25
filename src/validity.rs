1ef4101e8cbf252be3900c95d7b0a9e15c2fee3f
Paweł Łukasik <lukasik.pawel@gmail.com>
2024-02-25 13:24:14 +0100

    Implement valid func for incomplete boards

--- src/validity.rs
+++ src/validity.rs
@@ -1,11 +1,12 @@
 fn is_valid_board(board: Vec<Option<usize>>) -> bool {
     let mut result = true;
 
-    let n = board.len();
+    let number_complete_columns = board.iter().filter(|column| column.is_some())
+        .count();
 
-    let newest_queen_position = board[n - 1];
-    for y in 1..n {
-        if board[y] == newest_queen_position {
+    let newest_queen_position = board[number_complete_columns - 1].unwrap();
+    for x in 1..number_complete_columns {
+        if board[x].unwrap() == newest_queen_position {
             result = false;
             break;
         }
@@ -30,6 +31,15 @@ mod tests {
     #[test]
     fn given_adjacent_queens_should_return_invalid() {
         let board = vec![Some(1), Some(3), Some(0), Some(0)];
+
+        let result = is_valid_board(board);
+
+        assert_eq!(result, false)
+    }
+
+    #[test]
+    fn given_far_adjacent_queens_on_incomplete_board_should_return_invalid() {
+        let board = vec![Some(1), Some(3), Some(1), None];
 
         let result = is_valid_board(board);
 
